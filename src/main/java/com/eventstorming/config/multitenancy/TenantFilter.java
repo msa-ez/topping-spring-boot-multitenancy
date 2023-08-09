64537aef2609121cfa9a29d05e33ef5d2b68d2bc
SeongWon <59052363+seongwonyang@users.noreply.github.com>
2023-08-10 00:53:27 +0900

    Update TenantFilter.java

--- src/main/java/com/eventstorming/config/multitenancy/TenantFilter.java
+++ src/main/java/com/eventstorming/config/multitenancy/TenantFilter.java
@@ -2,23 +2,48 @@ path: {{name}}/{{{options.packagePath}}}/config/multitenancy
 ---
 package {{options.package}}.config.multitenancy;
 
-import {{options.package}}.security.AuthenticationService;
+//import newtest.security.AuthenticationService;
+import java.io.IOException;
+
+import javax.crypto.SecretKey;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
+
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
 
 @Component
 @Order(1)
 class TenantFilter implements Filter {
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response,
-      FilterChain chain) throws IOException, ServletException {
 
-        String tenant = AuthenticationService.getTenant((HttpServletRequest) request);
+    @Override
+    public void doFilter(
+        ServletRequest request,
+        ServletResponse response,
+        FilterChain chain
+    ) throws IOException, ServletException {
+
+        String token = ((HttpServletRequest)request).getHeader("Authorization");
+        if (token == null) {
+            throw new ServletException("No Token provided");
+        }
+
+        String tenant = JwtTokenParser.extractAudienceFromToken(token.replace("Bearer ", ""));
+    
         TenantContext.setCurrentTenant(tenant);
 
         try {
@@ -26,6 +51,5 @@ class TenantFilter implements Filter {
         } finally {
             TenantContext.setCurrentTenant("");
         }
-
     }
 }
